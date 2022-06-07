bedcaf7f4fac86b12a5df2e26bfa8ab2cd703477
jwiggi18 <jodiewiggins18@gmail.com>
2022-06-07 13:14:43 -0500

    functions and conditionals notes

---
+++ Rosalind/Intro Exercises.py
@@ -0,0 +1,33 @@
+python3
+"""
+problem
+Given: Two positive integers a and b, each less than 1000.
+
+Return: The integer corresponding to the square of the hypotenuse of the right triangle whose legs have lengths a and b.
+"""
+
+
+
+
+#hypotenuse calc c = sqrt{a^2 + b^2}
+#to get sqrt would:
+#from math import sqrt
+#import this
+#c = sqrt{a^2 + b^2}
+#then square c, which is undoing the sqrt
+
+#to get problem answer simply
+def hyp_sq(a,b):
+    sum = a + b
+    return sum
+
+
+def add_numbers(x, y):
+   sum = x + y
+   return sum
+
+
+print(add_numbers(5, 7)
+
+
+hypSq(5, 7)
