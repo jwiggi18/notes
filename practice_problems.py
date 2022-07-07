858c405b019ab98d38f9e7366d59df6d5534f1d2
jwiggi18 <jodiewiggins18@gmail.com>
2022-07-07 13:39:53 -0500

    slice and dice function

--- practice_problems.py
+++ practice_problems.py
@@ -48,3 +48,79 @@ def split_in_columns(message=MESSAGE):
        (pipe), return the obtained output string"""
     lines = message.split('\n')
     return '|'.join(lines)
+
+
+TEXT = """
+One really nice feature of Python is polymorphism: using the same operation
+on different types of objects.
+Let's talk about an elegant feature: slicing.
+You can use this on a string as well as a list for example
+'pybites'[0:2] gives 'py'.
+ The first value is inclusive and the last one is exclusive so
+here we grab indexes 0 and 1, the letter p and y.
+  When you have a 0 index you can leave it out so can write this as 'pybites'[:2]
+but here is the kicker: you can use this on a list too!
+['pybites', 'teaches', 'you', 'Python'][-2:] would gives ['you', 'Python']
+and now you know about slicing from the end as well :)
+keep enjoying our bites!
+"""
+
+#had hell with this one - 'return results` had to be a return of the first for loop`
+def slice_and_dice(text: str = TEXT) -> list:
+    """Get a list of words from the passed in text.
+       See the Bite description for step by step instructions"""
+    #iterate over the lines of the text split by line
+    results = []
+    for line in text.splitlines():
+        #strip whitespace from each line
+        clean = line.strip()
+        for line in clean.splitlines():
+            #check if the first character is lowercase,
+            if line[0].islower():
+                #split by word, extract the last word
+                lw = line.split().pop(-1)
+                #remove the last character if it is a period or exclaimation point
+                lws = lw.strip('.!')
+                #add the last word to the results list
+                results.append(lws)
+    return results
+
+#other way to do it - NOT MY WORK
+
+def slice_and_dice(text: str = TEXT) -> list:
+    """Get a list of words from the passed in text.
+       See the Bite description for step by step instructions"""
+    results = []
+    for line in text.strip().split("\n"):
+        if line.lstrip()[0].islower() == True:
+            results.append(line.split()[-1].strip(". !"))
+    return results
+
+
+def slice_and_dice(text: str = text) -> list:
+    """Get a list of words from the passed in text.
+       See the Bite description for step by step instructions"""
+    results = []
+    for raw_line in text.strip().splitlines():
+        if (line := raw_line.lstrip())[0] in ascii_lowercase:
+            results.append(line.split()[-1].rstrip('.!'))
+    return results
+
+
+"""If you use a single list comprehension, the results variable isn't really needed 
+because you can just return the resulting list. This solution is harder to read if you 
+haven't got your head around list comprehensions [they still make my head hurt a bit] but 
+splitting the list comprehension over three lines does help to make things clearer. 
+The first line is making the selection of the word and stripping off the unwanted "." or "!" where applicable. 
+The second line does what a for loop would do i.e. selects a line from our text. 
+The third line is the condition that decides whether the word is added to the list, 
+which depends on whether the line of text starts with a lowercase ASCII character or not. 
+I am not sure whether this is good practice or not. It is definitely compact, but readability is sacrificed."""
+
+
+def slice_and_dice(text: str = text) -> list:
+    """Get a list of words from the passed in text.
+       See the Bite description for step by step instructions"""
+    return [line.split()[-1].rstrip(".!")
+            for line in text.strip().split('\n')
+            if line.lstrip()[0] in ascii_lowercase]
